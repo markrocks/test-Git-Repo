f490e76d83d97678fe5903a0283914c5ce799fbf
Mark Martin <markhmartin@gmail.com>
2014-04-30 10:20:51 -0400

    added a test class

---
+++ MRTest.m
@@ -0,0 +1,13 @@
+//
+//  MRTest.m
+//  Git Test
+//
+//  Created by Mark Martin on 4/30/14.
+//  Copyright (c) 2014 Mark Rocks Software. All rights reserved.
+//
+
+#import "MRTest.h"
+
+@implementation MRTest
+
+@end
